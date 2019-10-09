6ed55b2cf7d6a4ae393a1b0d3395bbe4adc6ddf5
LeoMeloDev <56229298+LeoMeloDev@users.noreply.github.com>
2019-10-09 01:37:24 +0100

    Script indentation fix

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 (function($) {
-  //Tabs functionality
-  //Hide all content divs
-  $('#tabs div').hide();
-  //Show #tab1 content
-  $('#tabs div:first').show();
-  //Add 'active' class to the #tab1
-  $('#tabs ul li a:first').addClass('active');
-  //remove the active class from #tab1
-    $('#tabs ul li a').removeClass('active');
-    //Apply active class to selected tab
-    $(this).parent().addClass('active');
-    //Set currentTab to this link
-    var currentTab = $(this).attr('href');
-    //Hide away other tabs
+    //Tabs functionality
+    //Hide all content divs
     $('#tabs div').hide();
-    //and only show the selected tab
-    $(currentTab).show();
-    //Stop default link action from happening
-    return false;
-});
-});
+    //Show #tab1 content
+    $('#tabs div:first').show();
+    //Add 'active' class to the #tab1
+    $('#tabs ul li a:first').addClass('active');
+    //remove the active class from #tab1
+      $('#tabs ul li a').removeClass('active');
+      //Apply active class to selected tab
+      $(this).parent().addClass('active');
+      //Set currentTab to this link
+      var currentTab = $(this).attr('href');
+      //Hide away other tabs
+      $('#tabs div').hide();
+      //and only show the selected tab
+      $(currentTab).show();
+      //Stop default link action from happening
+      return false;
+    });
+  });
 
 })(window.jQuery);
