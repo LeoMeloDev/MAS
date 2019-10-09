ea410fd28660b971338985fb323be549a23d673f
LeoMeloDev <56229298+LeoMeloDev@users.noreply.github.com>
2019-10-09 01:33:29 +0100

    Function: adds 'active' class to #tab1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@
   $('#tabs div').hide();
   //Show #tab1 content
   $('#tabs div:first').show();
-
+  //Add 'active' class to the #tab1
+  $('#tabs ul li a:first').addClass('active');
 
 
 })(window.jQuery);
